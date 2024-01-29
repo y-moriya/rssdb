69a9e0a60b5cfa5e5f7d3db4c893f9e9f910ec26
Yu MORIYA <euro21st@gmail.com>
2024-01-29 18:29:21 +0900

    initial commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+import { Hono } from "https://deno.land/x/hono@v3.3.1/mod.ts";
+
+const app = new Hono();
+
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  const url = body.url;
+
+  const kv = await Deno.openKv();
+  const result = await kv.get([url]);
+  if (result.value) {
+    return c.json({ registered: true });
+  } else {
+    await kv.set([url], new Date().toISOString());
+    return c.json({ registered: false });
+  }
+})
+
+Deno.serve(app.fetch);
